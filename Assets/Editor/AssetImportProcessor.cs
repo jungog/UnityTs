a382f641e758f4ac3fbfa76fa1c79df0efcdf0b8
junegao <junegao@123u.com>
2022-06-03 15:12:21 +0800

    AssetImportProcessor代码优化

--- Assets/Editor/AssetImportProcessor.cs
+++ Assets/Editor/AssetImportProcessor.cs
@@ -11,20 +11,11 @@ namespace AssetImportProcessor
     {
         static void OnPostprocessAllAssets (string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths) 
         {
-            // UnityEngine.Debug.Log("static hello world tsaaaaaaaaaaaaaaaaaa");
-            // Debug.Log("static OnPostprocessAsset import settings for: " + importer);
-            // LogError("static hello world tsaaaaaaaaaaaaaaaaaa");
 		    // if (EditorTools.AvoidReimport) return;
-
             if (importedAssets.Length == 0)
             {
                 return;
             }
-            // foreach (string str in importedAssets)
-            // {
-            //      UnityEngine.Debug.Log("Reimported Asset: " + str);
-                 
-            // }
             for (int i = 0; i < importedAssets.Length; i++)
             {
                 if(importedAssets[i].Contains(".ts"))
@@ -38,65 +29,21 @@ namespace AssetImportProcessor
                     p.StartInfo.RedirectStandardOutput = true;
                     p.StartInfo.RedirectStandardError = false;
                     p.StartInfo.CreateNoWindow = true;
-            // string strOutput = null;
-            // UnityEngine.Debug.Log("pi.FileName: " + p.StartInfo.FileName);
+
                     try
                     {
-
                         p.Start();
-                        // p.StandardInput.WriteLine(cmd); //写入要执行的命令 可以多条写入也可以复合命令写入
-                        // p.StandardInput.WriteLine("D:\\cube\\Unity\\NewUnityTs\\Assets\\TsProj");
-                        // p.StandardInput.WriteLine("npm run build");
-                        // p.StandardInput.WriteLine("exit");
-                        // strOutput = p.StandardOutput.ReadToEnd();
                         p.WaitForExit();
                         p.Close();                         
                     }
                     catch (Exception e)
                     {
                         //异常处理
-                    UnityEngine.Debug.Log(" error" + e);
+                        UnityEngine.Debug.Log(" error" + e);
                     }
                     break;
                 }
-                // Debug.Log("Moved Asset: " + movedAssets[i] + " from: " + movedFromAssetPaths[i]);
-                // UnityEngine.Debug.Log("u Moved Asset: " + movedAssets[i] + " from: " + movedFromAssetPaths[i]);
             }
-
-/*string[] path = UnityEditor.AssetDatabase.FindAssets(_scriptName);
-if(path.Length>1)
-{
-Debug.LogError("有同名文件"+_scriptName+"获取路径失败");
-return null;
-}*/
-// string[] path = UnityEditor.AssetDatabase.FindAssets();
-// if(path.Length>1)
-// {
-// Debug.LogError("有同名文件"+_scriptName+"获取路径失败");
-// return null;
-// }
-
-
-
-
-
-
-
-
-
-
-
-            // foreach (string str in deletedAssets)
-            // {
-            //     Debug.Log("Deleted Asset: " + str);
-            //     UnityEngine.Debug.Log("u Deleted Asset: " + str);
-            // }
-
-            // for (int i = 0; i < movedAssets.Length; i++)
-            // {
-            //     Debug.Log("Moved Asset: " + movedAssets[i] + " from: " + movedFromAssetPaths[i]);
-            //     UnityEngine.Debug.Log("u Moved Asset: " + movedAssets[i] + " from: " + movedFromAssetPaths[i]);
-            // }
 		}
     }
 }
