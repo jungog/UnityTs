1fd37ec95a8ab9f97fcd726fcaeeea3edbc33832
junegao <junegao@123u.com>
2022-06-04 14:36:05 +0800

    删除ts文件时，自动删掉对应的js文件

--- Assets/Editor/AssetImportProcessor.cs
+++ Assets/Editor/AssetImportProcessor.cs
@@ -11,39 +11,54 @@ namespace AssetImportProcessor
     {
         static void OnPostprocessAllAssets (string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths) 
         {
-		    // if (EditorTools.AvoidReimport) return;
-            if (importedAssets.Length == 0)
+            // if (EditorTools.AvoidReimport) return;
+            //检测变更文件
+            if (importedAssets.Length != 0)
             {
-                return;
-            }
-            for (int i = 0; i < importedAssets.Length; i++)
-            {
-                if(importedAssets[i].Contains(".ts"))
+                for (int i = 0; i < importedAssets.Length; i++)
                 {
-                    UnityEngine.Debug.Log("Reimported Asset: " + importedAssets[i]);
-                    System.Diagnostics.Process p = new System.Diagnostics.Process();
-
-                    p.StartInfo.FileName = Application.dataPath+"/TsProj/npm_run_build.bat";
-                    p.StartInfo.UseShellExecute = false;
-                    p.StartInfo.RedirectStandardInput = true;
-                    p.StartInfo.RedirectStandardOutput = true;
-                    p.StartInfo.RedirectStandardError = false;
-                    p.StartInfo.CreateNoWindow = true;
-
-                    try
+                    if(importedAssets[i].EndsWith(".ts"))
                     {
-                        p.Start();
-                        p.WaitForExit();
-                        p.Close();                         
+                        UnityEngine.Debug.Log("Reimported Asset: " + importedAssets[i]);
+                        System.Diagnostics.Process p = new System.Diagnostics.Process();
+
+                        p.StartInfo.FileName = Application.dataPath+"/TsProj/npm_run_build.bat";
+                        p.StartInfo.UseShellExecute = false;
+                        p.StartInfo.RedirectStandardInput = true;
+                        p.StartInfo.RedirectStandardOutput = true;
+                        p.StartInfo.RedirectStandardError = false;
+                        p.StartInfo.CreateNoWindow = true;
+
+                        try
+                        {
+                            p.Start();
+                            p.WaitForExit();
+                            p.Close();                         
+                        }
+                        catch (Exception e)
+                        {
+                            //异常处理
+                            UnityEngine.Debug.Log(" error" + e);
+                        }
+                        break;
                     }
-                    catch (Exception e)
-                    {
-                        //异常处理
-                        UnityEngine.Debug.Log(" error" + e);
-                    }
-                    break;
                 }
             }
-		}
+
+            //检测删除文件
+            if (deletedAssets.Length != 0)
+            {
+                for (int i = 0; i < deletedAssets.Length; i++)
+                {
+                    if(deletedAssets[i].EndsWith(".ts"))
+                    {
+                        UnityEngine.Debug.Log("Deleted Asset: " + deletedAssets[i]);
+                        string[] path = deletedAssets[i].Split('/');
+                        AssetDatabase.DeleteAsset("Assets/Resources/"+path[path.Length-1].Replace(".ts",".js.txt"));
+                        AssetDatabase.DeleteAsset("Assets/TsProj/output/"+path[path.Length-1].Replace(".ts",".js"));
+                    }
+                }
+            }
+        }
     }
 }
